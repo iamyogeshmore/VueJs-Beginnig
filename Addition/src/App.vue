58a6812e42180d9fd5b04cdba9f7350873dd5583
Yogesh More <yogesh.more2098@gmail.com>
2023-06-17 15:38:55 +0530

    Added: Addition component template with calculator and taco selection functionality.

--- Addition/src/App.vue
+++ Addition/src/App.vue
@@ -1,15 +1,19 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png" />
   <HelloWorld msg="Hello Vue 3.0 + Vite" />
+  <Addition />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from "./components/HelloWorld.vue";
+import Addition from "./components/Addition.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
-</script>̥
+    HelloWorld,
+    Addition,
+  },
+};
+</script>
+̥
